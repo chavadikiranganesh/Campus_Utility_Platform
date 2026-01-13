d047564a194b9197d5580585c43915cd48003dca
chavadikiranganesh <chavadikiranganesh@gmail.com>
2026-01-13 13:47:58 +0530

    Fix Home page image path
    - Update image path to use correct Vite public folder reference
    - Change from 'public/images/...' to '/images/...' for proper serving

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -26,7 +26,7 @@ const Home: React.FC = () => {
           </div>
           <div className="flex-1">
              <img 
-               src="pages\images\ChatGPT Image Jan 13, 2026, 01_13_02 PM.png" 
+               src="/images/ChatGPT Image Jan 13, 2026, 01_13_02 PM.png" 
                alt="Campus Illustration" 
                className="rounded-2xl shadow-2xl border-4 border-white transform hover:scale-105 transition duration-500"
              />
